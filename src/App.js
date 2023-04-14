43052d0907923ccefaff0a4aae882ca397a2ac0e
Ligma Balls <d.ramos100402@gmail.com>
2023-04-14 08:46:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Algorithm from './components/Algorithm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3 id='message'>Enter your card number below.</h3>
+  <Algorithm></Algorithm>
+    </>
   );
 }
 
